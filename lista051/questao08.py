e2097a74a516c2bae17dfea22d169eed49f3ad4c
Beatriz Fontoura <befonto@gamil>
2023-08-22 11:43:08 -0300

    Questões finalizadas (com exceção da 15)

--- lista051/questao08.py
+++ lista051/questao08.py
@@ -7,8 +7,6 @@ cont = 0
 while (cont <= 20):
     a = cont % 2
     if (a == 1):
-        print(f"{cont} é um número ímpar.")
-    else:
-        print(f"{cont} não é um número ímpar.")
+        print(f"{cont} é um número ímpar")
 
     cont = cont + 1
--- lista051/questao15.py
+++ lista051/questao15.py
@@ -1,3 +1,18 @@
 '''
 Desenvolver um programa que apresente a série de Fibonacci até o décimo quinto termo. A série de Fibonacci é formada pela sequência 1,1,2,3,5,8,13,21,34, ... etc. Ela se caracteriza pela soma de um termo posterior com seu anterior subsequente.
 '''
+
+
+cont = 1
+
+ant = 0
+atual = 1
+
+while (cont < 610):
+    if (ant <= atual):
+        ant + atual
+
+    print(f"{}")
+
+    ant = atual
+    atual = ant
